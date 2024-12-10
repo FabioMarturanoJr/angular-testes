7f2fb4cbe8b7a3e9d835fe80f82ab843e67975ee
Fabio Marturano <fabiocmjor@gmail.com>
2024-12-10 10:31:17 -0300

    feat: manipulando estados e renderizando listas simples

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  showText = false;
 
-  updateShowText(value: boolean){
-    this.showText = value;
+  name = "Fulano";
+  showList = false;
+
+  carList = [
+    { id: 1, year: 1990, name: "Escort" }, 
+    { id: 2, year: 2000, name: "Civic Si" }, 
+    { id: 3, year: 2010, name: "Malibu" }, 
+    { id: 4, year: 2020, name: "Nivus" },]
+
+  updateShowList(value: boolean) {
+    this.showList = value;
   }
 }
