e2fc8e0660bbf7dcbcb214be82428268def5708e
Fabio Marturano <fabiocmjor@gmail.com>
2024-12-10 11:05:26 -0300

    feat: simple routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MainComponent } from './components/main/main.component';
 
 export const routes: Routes = [
     {
@@ -9,5 +10,9 @@ export const routes: Routes = [
     {
         path: "home",
         component: HomeComponent
+    },
+    {
+        path: "main",
+        component: MainComponent
     }
 ];
